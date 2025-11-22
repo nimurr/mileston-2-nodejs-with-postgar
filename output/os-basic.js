eb2dcc8ffcc65b55edec27f23f477a7b051bd07e
nimurr <nimurnerob404@gmail.com>
2025-11-22 20:25:23 +0600

    Add section headers to os-basic.js for better code organization

--- output/os-basic.js
+++ output/os-basic.js
@@ -2,7 +2,7 @@ const os = require("os");
 
 console.log("System info ");
 console.log('-'.repeat(50))
-
+//?----------- Basic Platform Information
 console.log("Platform Details :")
 console.log("Platform :", os.platform());
 console.log("Architecture :", os.arch());
@@ -11,7 +11,7 @@ console.log("OS Release :", os.release())
 console.log('Host Name :', os.hostname())
 
 
-
+//?----------- CPU and Memory Information
 console.log("\nCPU info :")
 console.log('-'.repeat(50))
 
@@ -23,6 +23,7 @@ console.log("CPU Speed :- ", cpus[0].speed, "MHz")
 const totalMem = os.totalmem() / (1024 ** 3); // Convert to GB
 const freeMem = os.freemem() / (1024 ** 3); // Convert to GB
 
+//?----------- Memory Information
 console.log("\nMemory info :")
 console.log('-'.repeat(50))
 console.log(`Total Memory : ${totalMem.toFixed(2)} GB`);
@@ -30,6 +31,8 @@ console.log(`Free Memory : ${freeMem.toFixed(2)} GB`);
 console.log(`Used Memory : ${(totalMem - freeMem).toFixed(2)} GB`);
 console.log(`Memory Usage : ${((totalMem - freeMem) / totalMem * 100).toFixed(2)} %`);
 
+
+//?----------- Network Information
 console.log("\nNetwork Interfaces :")
 console.log('-'.repeat(50))
 const networkInterfaces = os.networkInterfaces();
